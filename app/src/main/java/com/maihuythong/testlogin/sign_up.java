620097dcaf96cb3190480fe42f5f7e58a79d81e4
letrungtiennbk9 <letrungtiennbk9@gmail.com>
2019-11-08 14:34:08 +0700

    + them file sign_up.java va sign_up.xml

---
+++ app/src/main/java/com/maihuythong/testlogin/sign_up.java
@@ -0,0 +1,63 @@
+package com.maihuythong.testlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.maihuythong.testlogin.R;
+import com.maihuythong.testlogin.network.MyAPIClient;
+import com.maihuythong.testlogin.network.UserService;
+
+public class sign_up extends AppCompatActivity {
+    public static String TAG  = "LoginActivity";
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mSignUpFormView;
+    private ProgressDialog mProgressDialog;
+    private UserService userService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setCancelable(false);
+        userService = MyAPIClient.getInstance().getAdapter().create(UserService.class);
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.integer.signup || id == EditorInfo.IME_NULL) {
+                    //attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignUpButton = (Button) findViewById(R.id.email_sign_up_button);
+        mEmailSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //attemptLogin();
+            }
+        });
+
+        mSignUpFormView = findViewById(R.id.registration_form);
+        mProgressView = findViewById(R.id.signup_progress);
+    }
+}
