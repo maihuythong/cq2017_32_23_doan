68f6b8a7cee599e59799ad2b4f29e37194b9e62e
PrimeWalkerVN <nchithanh609@gmail.com>
2019-11-16 00:13:51 +0700

    + ! Update create tour activity

--- app/src/main/java/com/maihuythong/testlogin/CreateTourActivity.java
+++ app/src/main/java/com/maihuythong/testlogin/CreateTourActivity.java
@@ -4,8 +4,11 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,8 +18,7 @@ import java.util.Calendar;
 public class CreateTourActivity  extends AppCompatActivity {
     private EditText startDate;
     private EditText endDate;
-
-
+    private AutoCompleteTextView tourNameView;
     private String tourName;
     private Long startDateMilis;
     private Long endDateMilis;
@@ -30,8 +32,10 @@ public class CreateTourActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_tour);
 
+        tourNameView = (AutoCompleteTextView) findViewById(R.id.tourName);
         startDate = (EditText) findViewById(R.id.start_date);
         endDate = (EditText) findViewById(R.id.end_date);
+
 
         startDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,23 +50,41 @@ public class CreateTourActivity  extends AppCompatActivity {
             }
         });
 
+
+        Button CreateTourButton = (Button)findViewById(R.id.create_tour_button);
+
+        CreateTourButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tourName=tourNameView.getText().toString();
+            }
+        });
+
     }
 
     private void chooseDateStart(){
         final Calendar calendar = Calendar.getInstance();
+        //Get current date and time
         int dayCr = calendar.get(Calendar.DATE);
         int monthCr = calendar.get(Calendar.MONTH);
         int yearCr = calendar.get(Calendar.YEAR);
+        final int hourCr = calendar.get(Calendar.HOUR);
+        final int minuteCr = calendar.get(Calendar.MINUTE);
 
+
+        //Take time from DatePicker into calendar
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
                 calendar.set(year,month,dayOfMonth);
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/YYYY");
                 startDate.setText(simpleDateFormat.format(calendar.getTime()));
-
+                startDateMilis=calendar.getTimeInMillis();
             }
+
         },yearCr,monthCr,dayCr);
+
 
         datePickerDialog.show();
     }
@@ -78,6 +100,7 @@ public class CreateTourActivity  extends AppCompatActivity {
                 calendar.set(year,month,dayOfMonth);
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/YYYY");
                 endDate.setText(simpleDateFormat.format(calendar.getTime()));
+                endDateMilis=calendar.getTimeInMillis();
             }
         },yearCr,monthCr,dayCr);
 
