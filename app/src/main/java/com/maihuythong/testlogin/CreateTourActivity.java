a2c64e8cd154e30af2f3e7e82572f0fa2fc7089b
PrimeWalkerVN <nchithanh609@gmail.com>
2019-11-16 11:01:36 +0700

    + ! handle parameter required in create tour activity

--- app/src/main/java/com/maihuythong/testlogin/CreateTourActivity.java
+++ app/src/main/java/com/maihuythong/testlogin/CreateTourActivity.java
@@ -2,12 +2,15 @@ package com.maihuythong.testlogin;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,10 +22,26 @@ public class CreateTourActivity  extends AppCompatActivity {
     private EditText startDate;
     private EditText endDate;
     private AutoCompleteTextView tourNameView;
-    private String tourName;
+    private EditText adultsView;
+    private EditText childrenView;
+    private EditText minCostView;
+    private EditText maxCostView;
+    private ImageView chooseImageView;
+    private Button chooseImageButton;
+    private RadioButton isPrivateButton;
+
+
+
+
+    private String tourName ="";
     private Long startDateMilis;
     private Long endDateMilis;
-    private boolean isPrivate = true;
+    private int adults=0;
+    private int childs=0;
+    private int minCost;
+    private int maxCost;
+    private boolean isPrivate = false;
+    private String avatar ="";
 
 
 
@@ -35,6 +54,14 @@ public class CreateTourActivity  extends AppCompatActivity {
         tourNameView = (AutoCompleteTextView) findViewById(R.id.tourName);
         startDate = (EditText) findViewById(R.id.start_date);
         endDate = (EditText) findViewById(R.id.end_date);
+        adultsView = (EditText) findViewById(R.id.adults_id);
+        childrenView = (EditText) findViewById(R.id.children_id);
+        minCostView = (EditText) findViewById(R.id.min_cost_id);
+        maxCostView = (EditText) findViewById(R.id.max_cost_id);
+        chooseImageView = (ImageView) findViewById(R.id.imageView_id);
+        chooseImageButton = (Button) findViewById(R.id.chooseImage_button);
+        isPrivateButton= (RadioButton)findViewById(R.id.isPrivate_id);
+
 
 
         startDate.setOnClickListener(new View.OnClickListener() {
@@ -50,13 +77,38 @@ public class CreateTourActivity  extends AppCompatActivity {
             }
         });
 
+        isPrivateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPrivate =!isPrivate;
+                isPrivateButton.setChecked(isPrivate);
+            }
+        });
+
 
         Button CreateTourButton = (Button)findViewById(R.id.create_tour_button);
 
         CreateTourButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 tourName=tourNameView.getText().toString();
+                if(!TextUtils.isEmpty(adultsView.getText()))
+                adults = Integer.parseInt(adultsView.getText().toString());
+
+                if(!TextUtils.isEmpty(childrenView.getText()))
+                childs = Integer.parseInt(childrenView.getText().toString());
+
+                if(!TextUtils.isEmpty(minCostView.getText()))
+                minCost = Integer.parseInt(minCostView.getText().toString());
+
+                if(!TextUtils.isEmpty(maxCostView.getText()))
+                maxCost = Integer.parseInt(maxCostView.getText().toString());
+
+
+                //===========put extra to another activity hear============
+
+                //===========================================
             }
         });
 
@@ -68,8 +120,7 @@ public class CreateTourActivity  extends AppCompatActivity {
         int dayCr = calendar.get(Calendar.DATE);
         int monthCr = calendar.get(Calendar.MONTH);
         int yearCr = calendar.get(Calendar.YEAR);
-        final int hourCr = calendar.get(Calendar.HOUR);
-        final int minuteCr = calendar.get(Calendar.MINUTE);
+
 
 
         //Take time from DatePicker into calendar
@@ -106,4 +157,5 @@ public class CreateTourActivity  extends AppCompatActivity {
 
         datePickerDialog.show();
     }
+
 }
