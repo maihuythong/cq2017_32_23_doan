14f44db1fdff7d442bc24ba227af5fa9bfd1783c
letrungtiennbk9 <letrungtiennbk9@gmail.com>
2019-11-17 22:43:25 +0700

    ! Hoan chinh post stop point

--- app/src/main/java/com/maihuythong/testlogin/googlemapapi/StopPointGoogleMap.java
+++ app/src/main/java/com/maihuythong/testlogin/googlemapapi/StopPointGoogleMap.java
@@ -29,9 +29,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.facebook.login.Login;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationListener;
@@ -72,6 +83,8 @@ import com.maihuythong.testlogin.signup.APIService;
 import com.maihuythong.testlogin.signup.ApiUtils;
 //import com.mancj.materialsearchbar.MaterialSearchBar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -82,18 +95,21 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Response;
 
 
 public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, DirectionFinderListener, LocationListener {
 
-    public Number tourId;
     public static final int TEXT_REQUEST = 1;
+    private JSONArray stopPointsArray = new JSONArray();
+    private String addressFromMarker;
+    private double mLat, mLng;
 
     private static final String TAG = "MAP ACTIVITY";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -123,8 +139,6 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
     private EditText searchText;
     private ImageView myLocation;
 
-    private ArrayList<StopPointInfo> arrayStopPoint = new ArrayList<>();
-
     //private MaterialSearchBar materialSearchBar;
     PlacesClient placesClient;
 
@@ -136,14 +150,9 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
         setContentView(R.layout.activity_stop_point_google_map);
         //searchText = findViewById(R.id.input_search);
         myLocation = findViewById(R.id.my_location);
+//        materialSearchBar = findViewById(R.id.searchBar);
 
-        Bundle extras = getIntent().getExtras();
-        if(extras == null) {
-            tourId= null;
-        } else {
-            tourId= extras.getInt("tourId");
-            Log.d("Tour ID TAG: ","Here is tour ID: " + tourId);
-        }
+//        token = AutocompleteSessionToken.newInstance();
 
         if(!Places.isInitialized()){
             Places.initialize(getApplicationContext(),getResources().getString(R.string.google_maps_key));
@@ -151,8 +160,11 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
 
         placesClient = Places.createClient(this);
 
+
+
+
         getLocationPermission();        // check permission before init map
-        //init();
+        init();
     }
 
     private void init(){
@@ -250,6 +262,7 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
         if(autocompleteFragment != null) {
             autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.LAT_LNG, Place.Field.NAME));
 
+
             // Set up a PlaceSelectionListener to handle the response.
             autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
                 @Override
@@ -274,7 +287,12 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
             }
         });
 
+        //marker.showInfoWindow();
+        // map.moveCamera(CameraUpdateFactory.newLatLngZoom(KHTN,17));
+
+
         HideSoftKeyboard();
+
 
     }
 
@@ -299,7 +317,6 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
         }
 
     }
-
     private void initMap(){
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -353,28 +370,6 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
     }
 
     private void getLocationPermission(){
-//        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
-//                Manifest.permission.ACCESS_COARSE_LOCATION};
-//
-//        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
-//                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-//            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
-//                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-//                mLocationPermissionGranted = true;
-//         //       initMap();
-//            }else
-//            {
-//                ActivityCompat.requestPermissions(this,
-//                        permissions,
-//                        LOCATION_PERMISSION_REQUEST_CODE);
-//            }
-//        }else
-//        {
-//            ActivityCompat.requestPermissions(this,
-//                    permissions,
-//                    LOCATION_PERMISSION_REQUEST_CODE);
-//        }
-
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION};
 
@@ -383,53 +378,34 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
             if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
                     COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                 mLocationPermissionGranted = true;
-                initMap();
             }else{
                 ActivityCompat.requestPermissions(this,
                         permissions,
                         LOCATION_PERMISSION_REQUEST_CODE);
             }
-        }else{
+        }else
+        {
             ActivityCompat.requestPermissions(this,
                     permissions,
                     LOCATION_PERMISSION_REQUEST_CODE);
         }
+
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-//        mLocationPermissionGranted = false;
-//
-//        switch(requestCode){
-//            case LOCATION_PERMISSION_REQUEST_CODE:{
-//                if(grantResults.length > 0){
-//                    for (int i = 0; i < grantResults.length; ++i) {
-//                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-//                            mLocationPermissionGranted = false;
-//                            return;
-//                        }
-//                    }
-//                    mLocationPermissionGranted = true;
-//                    //if all permission granted => init map
-//                    initMap();
-//                }
-//            }
-//        }
 
         mLocationPermissionGranted = false;
 
         switch(requestCode){
             case LOCATION_PERMISSION_REQUEST_CODE:{
                 if(grantResults.length > 0){
-                    for(int i = 0; i < grantResults.length; i++){
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                    for (int i = 0; i < grantResults.length; ++i) {
+                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
                             mLocationPermissionGranted = false;
-                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
                             return;
                         }
                     }
-                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
                     mLocationPermissionGranted = true;
 
                     initMap();
@@ -462,6 +438,7 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
             init();
 
         }
+
 //        LatLng KHTN = new LatLng(10.762643, 106.682079);
 //        LatLng NHStreet = new LatLng(10.774467, 106.703274);
 //
@@ -518,10 +495,12 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
-    private String addressFromMarker;
+
 
     @Override
     public void onMapClick(LatLng latLng) {
+        mLat = latLng.latitude;
+        mLng = latLng.longitude;
         Log.d(TAG,"LAT: "+ latLng.latitude + " LONG: "+ latLng.longitude);
         // Toast.makeText(this,"LAT: "+ latLng.latitude + " LONG: ", Toast.LENGTH_SHORT).show();
         //desMarker = map.addMarker(new MarkerOptions()
@@ -561,7 +540,11 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
                 try {
                     addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
                     address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-
+//                    String city = addresses.get(0).getLocality();
+//                    String state = addresses.get(0).getAdminArea();
+//                    String country = addresses.get(0).getCountryName();
+//                    String postalCode = addresses.get(0).getPostalCode();
+//                    String knownName = addresses.get(0).getFeatureName();
                 }catch (IOException e){
                     Log.d(TAG,"Addresses error!" + e.getMessage());
                 }
@@ -571,6 +554,9 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
                 intent.putExtra("EXTRA_ADDRESS", address);
                 addressFromMarker = address;
                 startActivityForResult(intent, 1);
+
+
+
 
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
@@ -771,20 +757,89 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
                 //TODO:hihih
 //                stopPoints.setTourId("123123123");
 //                stopPoints.setStopPoints(arrayStopPoint);
-                APIService mAPIService = ApiUtils.getAPIService();
 
-                mAPIService.createStopPoints("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OSIsInBob25lIjoiMDgzMzUyNzQ1MCIsImVtYWlsIjoibWFpaHV5dGhvbmd4QGdtYWlsLmNvbSIsImV4cCI6MTU3NTk2OTE0NzMxNCwiYWNjb3VudCI6InVzZXIiLCJpYXQiOjE1NzMzNzcxNDd9.0WdtlhBt-5NHzKGRvtKKnoxhoM0vn3_0p4dJfkNRBjA","1329", arrayStopPoint).enqueue(new Callback<StopPoints>() {
-                    @Override
-                    public void onResponse(Call<StopPoints> call, Response<StopPoints> response) {
-                        Log.d("RESPONSE", response.body().toString());
-                        Log.d("xong", response.code() + "");
-                    }
 
+
+
+
+
+
+                RequestQueue requestQueue;
+
+                // Instantiate the cache
+                Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+                // Set up the network to use HttpURLConnection as the HTTP client.
+                Network network = new BasicNetwork(new HurlStack());
+
+                // Instantiate the RequestQueue with the cache and network.
+                requestQueue = new RequestQueue(cache, network);
+
+                // Start the queue
+                requestQueue.start();
+
+                String theUrl ="http://35.197.153.192:3000/tour/set-stop-points";
+
+                JSONObject req = new JSONObject();
+                try {
+                    Intent intent = getIntent();
+                    int tourId = intent.getIntExtra("tourId", 1387);
+                    String tourIdString = tourId + "";
+                    req.put("tourId", tourIdString);
+                    req.put("stopPoints", (Object) stopPointsArray);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, theUrl, req, new Response.Listener<JSONObject>() {
                     @Override
-                    public void onFailure(Call<StopPoints> call, Throwable throwable) {
-                        Log.d("khong xong", "Not OK");
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray b = new JSONArray();
+                            Log.d("kkk", response.getString("message"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
-                });
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("lll", error.toString());
+                    }
+                }){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        SharedPreferences sf = getSharedPreferences("com.maihuythong.testlogin_preferences", MODE_PRIVATE);
+                        String tok = sf.getString("login_access_token", "");
+                        params.put("Content-Type", "application/json");
+//                        params.put("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OSIsInBob25lIjoiMDgzMzUyNzQ1MCIsImVtYWlsIjoibWFpaHV5dGhvbmd4QGdtYWlsLmNvbSIsImV4cCI6MTU3NTk2OTE0NzMxNCwiYWNjb3VudCI6InVzZXIiLCJpYXQiOjE1NzMzNzcxNDd9.0WdtlhBt-5NHzKGRvtKKnoxhoM0vn3_0p4dJfkNRBjA");
+                        params.put("Authorization", tok);
+                        return params;
+                    }
+                };
+                requestQueue.add(jsonObjectRequest);
+
+
+//// Request a string response from the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.POST, theUrl, req,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                // Display the first 500 characters of the response string.
+//                                textView.setText("Response is: "+ response.substring(0,500));
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        textView.setText("That didn't work!");
+//                    }
+//                });
+//
+//// Add the request to the RequestQueue.
+//                queue.add(stringRequest);
         }
     }
 
@@ -811,22 +866,44 @@ public class StopPointGoogleMap extends AppCompatActivity implements OnMapReadyC
         // Test to make sure the intent reply result was good.
         if (resultCode == RESULT_OK) {
             String stopPointName = data.getStringExtra("REPLY_STOP_POINT_NAME");
-            //TODO:
-//            String serviceType = data.getStringExtra("REPLY_SERVICE_TYPE");
-//            String province = data.getStringExtra("REPLY_PROVINCE");
+            int serviceType = data.getIntExtra("REPLY_SERVICE_TYPE", 1);
+            int province = data.getIntExtra("REPLY_PROVINCE", 1);
+            int arrivalAt = data.getIntExtra("REPLY_ARRIVAL_AT", 1000000);
+            int leaveAt = data.getIntExtra("REPLY_LEAVE_AT", 2000000);
 
 
-            StopPointInfo stopPointInfo = new StopPointInfo();
-//            stopPointInfo.setAddress(addressFromMarker);
-            stopPointInfo.setName("hihi");
-            stopPointInfo.setLatitude(123);
-            stopPointInfo.setLongtitude(123);
-            stopPointInfo.setArrivalAt(123856);
-            stopPointInfo.setLeaveAt(545312);
-            stopPointInfo.setServiceTypeId(1);
-//            stopPointInfo.setProvinceId(1);
+//            StopPointInfo stopPointInfo = new StopPointInfo();
+////            stopPointInfo.setAddress(addressFromMarker);
+//            stopPointInfo.setName("hihi");
+//            stopPointInfo.setLatitude(123);
+//            stopPointInfo.setLongtitude(123);
+//            stopPointInfo.setArrivalAt(123856);
+//            stopPointInfo.setLeaveAt(545312);
+//            stopPointInfo.setServiceTypeId(1);
+////            stopPointInfo.setProvinceId(1);
+//
+//            Log.d("fff" ,stopPointInfo.toString());
+//
+//            arrayStopPoint.add(stopPointInfo);
 
-            arrayStopPoint.add(stopPointInfo);
+
+            JSONObject tmp = new JSONObject();
+            try{
+                tmp.put("name", stopPointName);
+                tmp.put("lat", mLat);
+                tmp.put("long", mLng);
+                //TODO:
+                tmp.put("leaveAt", 1000000);
+                tmp.put("arrivalAt", 2000000);
+                tmp.put("serviceTypeId", serviceType);
+                tmp.put("address", addressFromMarker);
+                stopPointsArray.put(tmp);
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
         }
     }
 
