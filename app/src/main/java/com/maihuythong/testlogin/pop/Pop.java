3c98f603e8990430ef3f245e5dc39e8fb17fb367
letrungtiennbk9 <letrungtiennbk9@gmail.com>
2019-11-17 10:11:07 +0700

    + Them file cho chuc nang popup

---
+++ app/src/main/java/com/maihuythong/testlogin/pop/Pop.java
@@ -0,0 +1,33 @@
+package com.maihuythong.testlogin.pop;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.maihuythong.testlogin.R;
+
+public class Pop extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
+        setContentView(R.layout.pop_layout);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*0.8), (int)(height*0.6));
+    }
+}
