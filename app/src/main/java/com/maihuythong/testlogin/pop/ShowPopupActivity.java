ab2dc1d3811d56d90642846e339c8b7abd479d0e
PrimeWalkerVN <nchithanh609@gmail.com>
2019-11-17 23:25:46 +0700

    ! add date time dialog for popup stop point

--- app/src/main/java/com/maihuythong/testlogin/pop/ShowPopupActivity.java
+++ app/src/main/java/com/maihuythong/testlogin/pop/ShowPopupActivity.java
@@ -1,23 +1,42 @@
 package com.maihuythong.testlogin.pop;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.maihuythong.testlogin.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class ShowPopupActivity extends AppCompatActivity {
 
     private Spinner spinnerProvince;
     private MenuItem itemSpinnerProvince;
+
+    private EditText arriveTimeView;
+    private EditText arriveDateView;
+    private EditText leaveTimeView;
+    private EditText leaveDateView;
+
+    private Number arriveAtMilis;
+    private Number leaveAtMilis;
+
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +62,123 @@ public class ShowPopupActivity extends AppCompatActivity {
         // Put that message into the text_message TextView
         TextView mAddressTextView = findViewById(R.id.address_id);
         mAddressTextView.setText(addressPassedFromMap);
+
+
+        arriveTimeView =findViewById(R.id.arrive_time_id);
+        arriveDateView=findViewById(R.id.arrive_date_id);
+        leaveTimeView=findViewById(R.id.leave_time_id);
+        leaveDateView=findViewById(R.id.leave_date_id);
+
+        arriveTimeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseArriveTime();
+            }
+        });
+
+        arriveDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseArriveDate();
+            }
+        });
+
+        leaveTimeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseLeaveTime();
+            }
+        });
+
+        leaveDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseLeaveDate();
+            }
+        });
+
     }
+
+    private void chooseArriveDate(){
+        final Calendar calendar = Calendar.getInstance();
+        //Get current date
+        int dayCr = calendar.get(Calendar.DATE);
+        int monthCr = calendar.get(Calendar.MONTH);
+        int yearCr = calendar.get(Calendar.YEAR);
+
+        //Take time from DatePicker into calendar
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                calendar.set(year,month,dayOfMonth);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/YYYY");
+                arriveDateView.setText(simpleDateFormat.format(calendar.getTime()));
+                arriveAtMilis=calendar.getTimeInMillis();
+            }
+
+        },yearCr,monthCr,dayCr);
+
+        datePickerDialog.show();
+    }
+
+
+    private  void chooseArriveTime(){
+        final Calendar calendar = Calendar.getInstance();
+        //get current time
+        int hourCr = calendar.get(Calendar.HOUR);
+        int minuteCr = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                arriveTimeView.setText(hourOfDay + ":" + minute);
+                arriveAtMilis = calendar.getTimeInMillis();
+            }
+        },hourCr,minuteCr,true);
+
+        timePickerDialog.show();
+    }
+
+    private  void chooseLeaveTime(){
+
+        final Calendar calendar = Calendar.getInstance();
+        //get current time
+        int hourCr = calendar.get(Calendar.HOUR);
+        int minuteCr = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                leaveTimeView.setText(hourOfDay + ":" + minute);
+                leaveAtMilis = calendar.getTimeInMillis();
+            }
+        },hourCr,minuteCr,true);
+        timePickerDialog.show();
+    }
+
+    private  void chooseLeaveDate(){
+        final Calendar calendar = Calendar.getInstance();
+        //Get current date
+        int dayCr = calendar.get(Calendar.DATE);
+        int monthCr = calendar.get(Calendar.MONTH);
+        int yearCr = calendar.get(Calendar.YEAR);
+
+        //Take time from DatePicker into calendar
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(year,month,dayOfMonth);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/YYYY");
+                leaveDateView.setText(simpleDateFormat.format(calendar.getTime()));
+                leaveAtMilis=calendar.getTimeInMillis();
+            }
+
+        },yearCr,monthCr,dayCr);
+
+        datePickerDialog.show();
+    }
+
 
 
     public void returnReply(View view) {
